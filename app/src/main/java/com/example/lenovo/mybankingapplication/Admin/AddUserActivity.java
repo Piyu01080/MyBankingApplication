7be47bd61a4d49c84aa12fe8ed461c3209b22beb
pooja <poojashende0108@gmail.com>
2018-01-20 13:02:06 +0530

    Test

---
+++ app/src/main/java/com/example/lenovo/mybankingapplication/Admin/AddUserActivity.java
@@ -0,0 +1,19 @@
+package com.example.lenovo.mybankingapplication.Admin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.lenovo.mybankingapplication.R;
+
+/**
+ * Created by Lenovo on 19-01-2018.
+ */
+
+public class AddUserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_user);
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/mybankingapplication/Constants.java
@@ -0,0 +1,8 @@
+package com.example.lenovo.mybankingapplication;
+
+/**
+ * Created by Lenovo on 19-01-2018.
+ */
+
+public class Constants {
+}
